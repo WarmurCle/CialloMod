662ad6354d487179caad92ebfe55c68a98d03db3
Polaris <2058770793@qq.com>
2026-05-07 20:25:17 +0800

    Ciallo sfx

--- ModInitializer.cs
+++ ModInitializer.cs
@@ -1,7 +1,12 @@
+using BaseLib.Audio;
 using CialloMod.src.Core.Models.Cards;
 using HarmonyLib;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using System;
+using static Godot.OpenXRCompositionLayer;
 
 namespace CialloMod
 {
@@ -10,23 +15,40 @@ namespace CialloMod
     {
         public static void Initialize()
         {
-            {
-                Parser.Parse();
-                int sum = 0;
-                foreach (var pair in Parser.Apps)
-                {
-                    foreach (bool value in pair.Value.AchievementsStatus)
-                    {
-                        if (value)
-                            sum++;
-                    }
-                }
-                CialloStrike.HitCount = sum;
-                var harmony = new Harmony("CialloMod.Polaris");
-                harmony.PatchAll();
 
-                ModHelper.AddModelToPool(typeof(ColorlessCardPool), typeof(CialloStrike));
+            Parser.Parse();
+            int sum = 0;
+            foreach (var pair in Parser.Apps)
+            {
+                foreach (bool value in pair.Value.AchievementsStatus)
+                {
+                    if (value)
+                        sum++;
+                }
             }
+            CialloStrike.HitCount = sum;
+            var harmony = new Harmony("CialloMod.Polaris");
+
+            var prefixMethod = new HarmonyMethod(Prefix);
+            harmony.Patch(typeof(SfxCmd).GetMethod("Play", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public, null, new Type[] { typeof(string), typeof(float) }, null), prefixMethod);
+            harmony.PatchAll();
+            ModHelper.AddModelToPool(typeof(ColorlessCardPool), typeof(CialloStrike));
+
+        }
+
+        [HarmonyPrefix]
+        public static bool Prefix(string sfx, float volume)
+        {
+            if(sfx == "RANDOMCIALLOSND")
+            {
+                sfx = $"res://CialloMod/assets/audio/cia{new Random().Next(10).ToString()}.mp3";
+            }
+            if (sfx.StartsWith("res://CialloMod"))
+            {
+                new AutoModAudio("res://").PlaySfx(sfx.Replace("res://CialloMod/", ""), 0, volume, 0, 1).PitchScale = 1;
+                return false;
+            }
+            return true;
         }
     }
 }
--- src/Core/Models/Cards/CialloStrike.cs
+++ src/Core/Models/Cards/CialloStrike.cs
@@ -1,12 +1,28 @@
+using BaseLib.Audio;
+using Godot;
+using GodotPlugins.Game;
+using MegaCrit.Sts2.Core.Audio.Debug;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.Hooks;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Monsters;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Random;
 using MegaCrit.Sts2.Core.ValueProps;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using static Godot.HttpRequest;
 
 namespace CialloMod.src.Core.Models.Cards
 {
@@ -29,10 +45,10 @@ namespace CialloMod.src.Core.Models.Cards
         protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
         {
             await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).WithHitCount(HitCount).FromCard(this).Targeting(cardPlay.Target)
-                .WithHitFx("vfx/vfx_attack_slash")
+                .WithHitFx("vfx/vfx_attack_slash", "RANDOMCIALLOSND")
                 .Execute(choiceContext);
         }
-
+        
         protected override void OnUpgrade()
         {
             base.DynamicVars.Damage.UpgradeValueBy(2m);
